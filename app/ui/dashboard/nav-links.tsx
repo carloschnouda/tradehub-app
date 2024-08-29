70fbd66fd891c3e2fcd81dc75dc92aade373410f
carloschnouda <carlos.chnouda@gmail.com>
2024-08-29 17:42:09 +0300

    add ative page link - Dashboard

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -8,7 +8,11 @@ import { usePathname } from 'next/navigation';
 import clsx from 'clsx';
 
 const links = [
-  { name: 'Dashboard', href: '/dashboard', icon: HomeModernIcon },
+  {
+    name: 'Dashboard',
+    href: '/dashboard',
+    icon: HomeModernIcon
+  },
   {
     name: 'Invoices',
     href: '/dashboard/invoices',
@@ -18,18 +22,20 @@ const links = [
 
 export default function NavLinks() {
   const pathname = usePathname();
+
   return (
     <>
       {links.map((link) => {
         const LinkIcon = link.icon;
         return (
+          console.log(pathname === link.href),
           <Link
             key={link.name}
             href={link.href}
             className={clsx(
               "text-wh flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-neutral-900 p-3 text-sm font-medium text-white hover:bg-neutral-600 hover:text-white md:flex-none md:justify-start md:p-2 md:px-3",
               {
-                'bg-neutral-600': pathname === link.href,
+                'bg-neutral-500': pathname === link.href,
               },
             )}
           >
